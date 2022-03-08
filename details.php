e90cf33feef77342c42c268bcaa70cc1fa9ce930
Jake Tyler Simon <jtsimonbaseball@gmail.com>
2022-03-07 21:10:52 -0800

    pizza update

--- details.php
+++ details.php
@@ -1,6 +1,34 @@
 <?php 
 
+include('config/db_connect.php');
 
+if(isset($_POST['delete'])) {
+    
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+    $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+    if(mysqli_query($conn, $sql)) {
+        header('Location: index.php');
+    } {
+        echo 'query error: '.mysqli_error($conn);
+    }
+}
+
+if(isset($_GET['id'])) {
+
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
+
+    $sql = "SELECT * FROM pizzas WHERE id = $id";
+
+    $result = mysqli_query($conn, $sql);
+
+    $pizza = mysqli_fetch_assoc($result);
+
+    mysqli_free_result($result);
+    mysqli_close($conn);
+
+}
 
 ?>
 
@@ -8,6 +36,28 @@
 <html>
 
     <?php include('templates/header.php') ?>
+
+    <div class="container center">
+        <?php if($pizza): ?>
+
+            <h4><?php echo htmlspecialchars($pizza['title']); ?></h4>
+            <p>Created by: <?php echo htmlspecialchars($pizza['email']); ?></p>
+            <p><?php echo date($pizza['created_at']); ?></p>
+            <h5>Ingredients</h5>
+            <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+            <form action="details.php" method="POST">
+                <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id'] ?>">
+                <input type="submit" name="delete" value="Delete" class="btn brand">
+            </form>
+
+        <?php else: ?>
+            <h5>No such pizzas exists!</h5>
+
+        <?php endif; ?>
+
+
+    </div>
 
     <?php include('templates/footer.php') ?>
 
