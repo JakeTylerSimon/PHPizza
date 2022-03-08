fbd048984e5b90e46eb418db5bec8d36266d628a
Jake Tyler Simon <jtsimonbaseball@gmail.com>
2022-03-07 19:39:21 -0800

    pizza rendering

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 
+    $conn = mysqli_connect('localhost', 'Jake', 'Test1234', 'phpizza_shop');
+
+    if(!$conn) {
+        echo 'Connecion error: '.mysqli_connect();
+    }
+
+    $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+
+    $result = mysqli_query($conn, $sql);
+
+    $pizzas= mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    mysqli_free_result($result);
+
+    mysqli_close($conn);
+   
+    explode(',', $pizzas[0]['ingredients']);
 
 ?>
 
@@ -8,5 +25,30 @@
 
     <?php include('templates/header.php') ?>
     
+    <h4 class="center grey-text">Pizzas!</h4>
+    <div class="container">
+        <div class="row">
+            
+            <?php foreach($pizzas as $pizza): ?>
+                <div class="col s6 md3">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                            <ul>
+                                <?php foreach(explode(',', $pizza['ingredients']) as $ing): ?>
+                                    <li><?php echo htmlspecialchars($ing); ?></li>
+                                <?php endforeach ?>
+                            </ul>
+                        </div>
+                        <div class="card-action right-align">
+                            <a href="#" class="brand-text">more info</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+
+        </div>
+    </div>
+
     <?php include('templates/footer.php') ?>
 </html>
